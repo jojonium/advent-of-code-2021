daacd7251d58b8a69565f19f616c6a1d7736cfa2
Joseph Petitti <joseph@petitti.org>
2021-12-01 14:07:13 -0500

    Remove unnecessary initializers

--- day01.sh
+++ day01.sh
@@ -3,20 +3,15 @@
 INPUT="${1:-inputs/day01.txt}"
 
 awk '
-BEGIN {cnt = 0}
-
 NR == 1 {last = $0}
-
 NR != 1 {
   if ($0 > last) cnt++
   last = $0
 }
-
 END {print "Part 1:", cnt}
 ' $INPUT
 
 awk '
-BEGIN {cnt = 0}
 NR == 1 {c = $0}
 NR == 2 {b = $0}
 NR == 3 {a = $0}
