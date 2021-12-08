b25f5456b538437c97dbebef70d17270108b4e9d
Joseph Petitti <joseph@petitti.org>
2021-12-08 10:59:50 -0500

    Clean up day 8

--- day08.ts
+++ day08.ts
@@ -15,12 +15,12 @@ const inputFile = process.argv[2] ?? 'inputs/day08.txt';
         )
     );
 
-  console.log('Part 1: ' + entries.reduce((sum, entry) => {
-    return sum + entry[1]!!.filter(w => [2, 3, 4, 7].includes(w.length)).length
-  }, 0));
+  console.log('Part 1: ' + entries.reduce((sum, entry) =>
+    sum + entry[1]!!.filter(w => [2, 3, 4, 7].includes(w.length)).length, 0)
+  );
 
   console.log('Part 2: ' + entries.reduce((sum, entry) => {
-    const map: {[key: string]: number | undefined} = {};
+    const map: {[key: string]: number} = {};
     const words = entry[0]!!.sort((a, b) => a.length - b.length);
     map[words[0]!!] = 1;
     map[words[1]!!] = 7;
@@ -28,29 +28,31 @@ const inputFile = process.argv[2] ?? 'inputs/day08.txt';
     map[words[9]!!] = 8;
 
     // Six segments.
-    for (const remaining of words.slice(6, 9)) {
+    words.slice(6, 9).forEach(remaining => {
       const missing = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
         .filter(l => !remaining.includes(l))[0]!!;
-      const c = Number(words[0]!!.includes(missing)) +
-        Number(words[1]!!.includes(missing)) +
-        Number(words[2]!!.includes(missing));
+      // Number of times the missing segment appears in 1, 7, and 4.
+      const c = [0, 1, 2].reduce((c, n) =>
+        c + Number(words[n]!!.includes(missing)), 0
+      );
       if (c === 0) map[remaining] = 9;
       if (c === 1) map[remaining] = 0;
       if (c === 3) map[remaining] = 6;
-    }
+    });
 
     // Five segments.
-    for (const remaining of words.slice(3, 6)) {
+    words.slice(3, 6).forEach(remaining => {
       const missing = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
         .filter(l => !remaining.includes(l));
-      const c =
-        missing.reduce((sum, m) => sum + Number(words[0]!!.includes(m)), 0) +
-        missing.reduce((sum, m) => sum + Number(words[1]!!.includes(m)), 0) +
-        missing.reduce((sum, m) => sum + Number(words[2]!!.includes(m)), 0);
+      // Total number of times the missing segments appears in 1, 7, and 4.
+      const c = [0, 1, 2].reduce((c, n) =>
+        c + missing.reduce((sum, m) => sum + Number(words[n]!!.includes(m)), 0),
+        0
+      );
       if (c === 1) map[remaining] = 3;
       if (c === 4) map[remaining] = 2;
       if (c === 3) map[remaining] = 5;
-    }
+    });
 
     return sum + Number(entry[1]!!.reduce((acc, w) => acc + map[w]!!, ""));
   }, 0));
