fd0bab44caef87799d57dd4af39124125b332df0
Joseph Petitti <joseph@petitti.org>
2021-12-10 09:52:57 -0500

    Clean up day 10

--- day10.ts
+++ day10.ts
@@ -8,50 +8,39 @@ const inputFile = process.argv[2] ?? 'inputs/day10.txt';
     .trim()
     .split('\n');
 
-  const {corrupted, incomplete} = lines.map((line) =>
-    line.split('').reduce((state, char) =>
-      ({
-        '[': () => ({...state, stack: state.stack.concat(['['])}),
-        '(': () => ({...state, stack: state.stack.concat(['('])}),
-        '{': () => ({...state, stack: state.stack.concat(['{'])}),
-        '<': () => ({...state, stack: state.stack.concat(['<'])}),
-        ']': () => ({
-          ...state,
-          score: (state.score === 0 && state.stack.pop() !== '[')
-            ? 57
-            : state.score
-        }),
-        ')': () => ({
-          ...state,
-          score: (state.score === 0 && state.stack.pop() !== '(')
-            ? 3
-            : state.score
-        }),
-        '}': () => ({
-          ...state,
-          score: (state.score === 0 && state.stack.pop() !== '{')
-            ? 1197
-            : state.score
-        }),
-        '>': () => ({
-          ...state,
-          score: (state.score === 0 && state.stack.pop() !== '<')
-            ? 25137
-            : state.score
-        }),
-      })[char]!!(),
-      {stack: new Array<string>(), score: 0})
-  ).reduce((answers, lineInfo) => ({
-    ...answers,
-    corrupted: answers.corrupted + lineInfo.score,
-    incomplete: answers.incomplete.concat(
-      (lineInfo.score === 0)
-        ? [lineInfo.stack.reduceRight((sum, char) =>
-          sum * 5 + {'(': 1, '[': 2, '{': 3, '<': 4}[char]!!, 0)
-        ]
+  const {corrupted, incomplete} = lines.reduce((answers, line) => {
+    const {score, stack} = line.split('').reduce((state, char) => ({
+      '[': () => ({...state, stack: state.stack.concat([char])}),
+      '(': () => ({...state, stack: state.stack.concat([char])}),
+      '{': () => ({...state, stack: state.stack.concat([char])}),
+      '<': () => ({...state, stack: state.stack.concat([char])}),
+      ']': () => ({
+        ...state,
+        score: state.score || (state.stack.pop() !== '[') ? 57 : 0
+      }),
+      ')': () => ({
+        ...state,
+        score: state.score || (state.stack.pop() !== '(') ? 3 : 0
+      }),
+      '}': () => ({
+        ...state,
+        score: state.score || (state.stack.pop() !== '{') ? 1197 : 0
+      }),
+      '>': () => ({
+        ...state,
+        score: state.score || (state.stack.pop() !== '<') ? 25137 : 0
+      }),
+    })[char]!!(),
+      {stack: new Array<string>(), score: 0}
+    );
+    return {
+      corrupted: answers.corrupted + score,
+      incomplete: answers.incomplete.concat((score === 0)
+        ? [stack.reduceRight((sum, char) => sum * 5 + ' ([{<'.indexOf(char), 0)]
         : []
-    )
-  }), {corrupted: 0, incomplete: new Array<number>()})
+      )
+    };
+  }, {corrupted: 0, incomplete: new Array<number>()});
 
   console.log('Part 1: ' + corrupted);
   console.log('Part 2: ' +
