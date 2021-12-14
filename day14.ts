653f7c17480c1cd2575decafac0dd52950dff26b
Joseph Petitti <joseph@petitti.org>
2021-12-14 10:21:52 -0500

    Add day 14 in TypeScript

---
+++ day14.ts
@@ -0,0 +1,47 @@
+import {promises as fsPromises} from 'fs';
+
+const inputFile = process.argv[2] ?? 'inputs/day14.txt';
+
+const run = (line: string, map: Map<string, string>, steps: number): number => {
+  let pairCounts = line.split('').reduce((pc, char, i, arr) => {
+    if (i === arr.length - 1) return pc;
+    pc[char + arr[i + 1]!!] = (pc[char + arr[i + 1]!!] ?? 0) + 1;
+    return pc;
+  }, {} as {[key: string]: number});
+
+  for (let i = 0; i < steps; ++i) {
+    const npc = {} as {[key: string]: number};
+    for (const pair in pairCounts) {
+      const newPair1 = pair.substring(0, 1) + map.get(pair);
+      const newPair2 = map.get(pair) + pair.substring(1);
+      npc[newPair1] = (npc[newPair1] ?? 0) + pairCounts[pair]!!;
+      npc[newPair2] = (npc[newPair2] ?? 0) + pairCounts[pair]!!;
+    }
+    pairCounts = npc;
+  }
+  const letterCounts = Object.entries(pairCounts).reduce((lc, entry) => {
+    for (const char of entry[0].split('')) {
+      lc[char] = (lc[char] ?? 0) + entry[1];
+    }
+    return lc;
+  }, {} as {[key: string]: number});
+  const sorted = Object.entries(letterCounts).sort((a, b) => a[1] - b[1]);
+  // Each letter belongs to two different pairs and therefore gets counted
+  // twice, EXCEPT for the first and last letter. The answer will either be
+  // this number, n-1, or n-2.
+  return Math.ceil((sorted[sorted.length - 1]!![1] / 2) - (sorted[0]!![1] / 2));
+};
+
+(async () => {
+  const [tStr, iStr] = (await fsPromises.readFile(inputFile))
+    .toString().split('\n\n').map(s => s.trim());
+  const template = tStr!!;
+  const instructions = iStr!!.split('\n').reduce((map, line) => {
+    const [left, right] = line.trim().split(' -> ');
+    map.set(left!!, right!!);
+    return map;
+  }, new Map<string, string>());
+
+  console.log('Part 1: ' + run(template, instructions, 10));
+  console.log('Part 2: ' + run(template, instructions, 40));
+})();
